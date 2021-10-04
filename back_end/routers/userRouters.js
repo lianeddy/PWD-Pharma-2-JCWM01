55cff09b314bbceba39190761782900d938e6f44
asodikin <achmadsodikin.xrpl2.16@gmail.com>
2021-10-04 20:31:36 +0700

    belum selesai

--- back_end/routers/userRouters.js
+++ back_end/routers/userRouters.js
@@ -1,10 +1,11 @@
 const express = require('express')
-const {usersController} = require('../controllers')
+const {userController} = require('../controllers')
 const {auth} = require('../helper/authToken')
 const routers = express.Router()
 
-
-routers.post('/regis', usersController.addData)
-
+routers.post('/login', userController.getData)
+routers.post('/register', userController.addData)
+routers.patch('/edit', userController.editData)
+routers.patch('/verified', auth, userController.verification)
 
 module.exports = routers
