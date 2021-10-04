c4a3e221073e6603c0236c760d339504a95339bc
asodikin <achmadsodikin.xrpl2.16@gmail.com>
2021-10-04 21:49:46 +0700

    dari functional comp ke class

--- front_end/src/App.js
+++ front_end/src/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
-import RegisterPage from './pages/auth/registerPage'
+import RegisterPage from './pages/auth/RegisterPage'
 import {BrowserRouter, Route, Switch} from 'react-router-dom'
 
 function App() {
--- front_end/src/pages/auth/registerPage.jsx
+++ front_end/src/pages/auth/registerPage.jsx
@@ -1,120 +1,97 @@
 import Axios from 'axios'
-import React, {useState, useEffect} from 'react'
+import React from 'react'
 import {URL_API} from '../../helper'
 
 
-const RegisterPage = () =>{
-    const [ username, setUsername] = useState('')
-    const [ email, setEmail] = useState('')
-    const [ password, setPassword] = useState('')
-    const [ confPassword, setConfPassword] = useState('')
-
-    const changeUsername = (e) =>{
-        const value = e.target.value 
-        setUsername(value)
-    }
-
-    const changeEmail = (e) =>{
-        const value = e.target.value 
-        setEmail(value)
-    }
-
-    const changePassword = (e) =>{
-        const value = e.target.value 
-        setPassword(value)
-    }
-
-    const changeConfPassword = (e) =>{
-        const value = e.target.value 
-        setConfPassword(value)
-    }
-
-
-    const onBtnRegister = (data) =>{
-        let un = username
-        let em = email
-        let pass = password
-        let conf = confPassword
-        console.log(un, pass);
-        
-
-        if (un == "" || em == "" || pass == ""){
-            alert('fill in all the form')
-        
-        }else if(pass !== conf){
-            alert("password tidak sesuai")
-
-        }else {
-            Axios.post(URL_API + '/user/register',{
-                un,
-                em,
-                pass
-            })
-            .then(res => console.log(res.data))
-            .catch(err => console.log(err))
+class RegisterPage extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            alertShow: 'none',
+            redirect: false
         }
-        
     }
+
+
    
 
-    
+    onBtnRegister = () => {
+        let username = this.regisUsername.value
+        let email = this.regisEmail.value
+        let password = this.regisPass.value
+        let confPassword = this.regisConfPass.value
+        console.log(username, password)
+
+        if (username == "" || email == "" || password == "") {
+            alert('Fill in all the form')
+        } else {
+            Axios.post(URL_API + '/user/regis', {
+                username, email, password
+            })
+                .then(res => console.log(res.data))
+                .catch(err => console.log(err))
+        }
+    }
+        
+
+      
 
     
-    return (
-        <div className="container" >
-            <div className="row">
-                <div className="col-12 text-center my-5" style={{backgroundColor:"grey"}}>
-                    <h1>Register page</h1>
+
+    render(){
+        return (
+            <div className="container" >
+                <div className="row">
+                    <div className="col-12 text-center my-5" style={{backgroundColor:"grey"}}>
+                        <h1>Register page</h1>
+                    </div>
                 </div>
-            </div>
-            <div className="row mt-5">
-                <div className="col-4 offset-4">
-                    <div className="card">
-                        <div className="card-body">
-                            <h5 className="font-weight-bold mb-3">register</h5>
-                            
-                            <input type="text"
-                            
-                            placeholder="username"
-                            className="form-control my-4"
-                            onChange={changeUsername}
-                            value={username}
-                            
-                            />
-                            <input type="text"
-                            placeholder="email"
-                            className="form-control my-4"
-                            onChange={changeEmail}
-                            value={email}
-                           
-                            />
-                            <input type="password"
-                            placeholder="password"
-                            className="form-control my-4"
-                            onChange={changePassword}
-                            value={password}
-
-                            
-                            />
-                            <input type="password"
-                            placeholder="password"
-                            className="form-control my-4"
-                            onChange={changeConfPassword}
-                            value={confPassword}
-                          
-                            />
-                            <div className="d-flex flex-row justify-content-between align-items-center">
-                                <button onClick={onBtnRegister} className="btn btn-primary mt-2">
-                                    REGISTER
-                                </button>
+                <div className="row mt-5">
+                    <div className="col-4 offset-4">
+                        <div className="card">
+                            <div className="card-body">
+                                <h5 className="font-weight-bold mb-3">register</h5>
+                                
+                                <input type="text"
+                                
+                                placeholder="username"
+                                className="form-control my-4"
+                                ref={el => this.regisUsername = el}
+                                
+                                />
+                                <input type="text"
+                                placeholder="email"
+                                className="form-control my-4"
+                                ref={el => this.regisEmail = el}
+                               
+                                />
+                                <input type="password"
+                                placeholder="password"
+                                className="form-control my-4"
+                                ref={el => this.regisPass = el}
+    
+                                
+                                />
+                                <input type="password"
+                                placeholder="password"
+                                className="form-control my-4"
+                                ref={el => this.regisConfPass = el}
+                              
+                                />
+                                <div className="d-flex flex-row justify-content-between align-items-center">
+                                    <button onClick={this.onBtnRegister} className="btn btn-primary mt-2">
+                                        REGISTER
+                                    </button>
+                                </div>
                             </div>
                         </div>
                     </div>
+                
                 </div>
-            
             </div>
-        </div>
-        
-    )
+            
+        )
+    }
+    
 }
 export default RegisterPage
