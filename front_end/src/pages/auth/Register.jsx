4cc5f6db587bc43024e06e00d8ac727a6273f10c
aurelsatria <84651555+aurelsatria@users.noreply.github.com>
2021-10-05 14:48:21 +0700

    Change Register into Class Component

--- front_end/src/pages/auth/Register.jsx
+++ front_end/src/pages/auth/Register.jsx
@@ -1,106 +1,123 @@
 import Axios from 'axios'
-import React, {useState, useEffect} from 'react'
-import { URL_API } from '../../helper';
+import React from 'react'
+import { URL_API } from '../../helper'
+import { connect } from 'react-redux'
+// import { registerUser } from '../../redux/actions/user'
 
 
-const Register = () =>{
-    const [ username, setUsername] = useState('')
-    const [ email, setEmail] = useState('')
-    const [ password, setPassword] = useState('')
-    const [ confPassword, setConfPassword] = useState('')
-
-    const changeUsername = (e) =>{
-        const value = e.target.value 
-        setUsername(value)
+class Register extends React.Component {
+    state = {
+       username : "",
+       email : "",
+       password : "",
+       confPassword : ""
     }
 
-    const changeEmail = (e) =>{
-        const value = e.target.value 
-        setEmail(value)
-    }
-
-    const changePassword = (e) =>{
-        const value = e.target.value 
-        setPassword(value)
-    }
-
-    const changeConfPassword = (e) =>{
-        const value = e.target.value 
-        setConfPassword(value)
-    }
+    inputHandler = (event) => {
+        const value = event.target.value;
+        const name = event.target.name;
+    
+        this.setState({ [name]: value });
+      };
 
 
-    const onBtnRegister = (data) =>{
-        let un = username
-        let em = email
-        let pass = password
-        console.log(un, em);
-
-        if (un == "" || em == "" || pass == ""){
-            alert('fill in all the form')
-        } else {
-            Axios.post(URL_API + '/user/regis', {
-                un, em, password
-            })
-            .then(res => console.log(res.data))
-            .catch(err => console.log(err))
-        }
-    }
    
-    return (
-        <div className="container" >
-            <div className="row">
-                <div className="col-12 text-center my-5" style={{backgroundColor:"grey"}}>
-                    <h1>Register page</h1>
+
+    onBtnRegister = () => {
+        const {username, email, password, confPassword } = this.state;
+        console.log(username, email, password, confPassword);
+
+        if(username == "" || email == "" || password == "" || confPassword == ""){
+            alert('fill in all the form')
+        }else if(password !== confPassword){
+            alert('password is not match')
+        }else{
+            Axios.post(`${URL_API}/user/register`, {
+                username,
+                email,
+                password,
+                confPassword
+              })
+                   //proses asyncronus
+                .then(() => {
+                  alert("berhasil mendapatkan users");
+                })
+                .catch(() => {
+                  alert("gagal mendapatkan users");
+                });
+        }
+  
+    }
+        
+
+       
+
+    
+
+    render(){
+        return (
+            <div className="container " style={{backgroundColor:"grey"}} >
+                <div className="row">
+                    <div className="col-12 text-center my-5" >
+                        <h1>Register page</h1>
+                    </div>
                 </div>
-            </div>
-            <div className="row mt-5">
-                <div className="col-4 offset-4">
-                    <div className="card">
-                        <div className="card-body">
-                            <h5 className="font-weight-bold mb-3">register</h5>
-                            
-                            <input type="text"
-                            
-                            placeholder="username"
-                            className="form-control my-4"
-                            onChange={changeUsername}
-                            value={username}
-                            
-                            />
-                            <input type="text"
-                            placeholder="email"
-                            className="form-control my-4"
-                            onChange={changeEmail}
-                            value={email}
-                           
-                            />
-                            <input type="text"
-                            placeholder="username"
-                            className="form-control my-4"
-                            onChange={changePassword}
-                            value={password}
-                            
-                            />
-                            <input type="text"
-                            placeholder="username"
-                            className="form-control my-4"
-                            onChange={changeConfPassword}
-                            value={confPassword}
-                          
-                            />
-                            <div className="d-flex flex-row justify-content-between align-items-center">
-                                <button onClick={onBtnRegister} className="btn btn-primary mt-2">
-                                    REGISTER
-                                </button>
+                <div className="row mt-5">
+                    <div className="col-4 offset-4">
+                        <div className="card">
+                            <div className="card-body">
+                                <h5 className="font-weight-bold mb-3">register</h5>
+                                
+                                        <input
+                                        name="username"
+                                        onChange={this.inputHandler}
+                                        placeholder="username"
+                                        type="text"
+                                        className="form-control my-2"
+                                        />
+                                        <input
+                                        name="email"
+                                        onChange={this.inputHandler}
+                                        placeholder="email"
+                                        type="text"
+                                        className="form-control my-2"
+                                        />
+                                        <input
+                                        name="password"
+                                        onChange={this.inputHandler}
+                                        placeholder="password"
+                                        type="password"
+                                        className="form-control my-2"
+                                        />
+                                         <input
+                                        name="confPassword"
+                                        onChange={this.inputHandler}
+                                        placeholder="configuration password"
+                                        type="password"
+                                        className="form-control my-2"
+                                        />
+                                <div className="d-flex flex-row justify-content-between align-items-center">
+                                    <button onClick={this.onBtnRegister} className="btn btn-primary mt-2">
+                                        REGISTER
+                                    </button>
+                                </div>
                             </div>
                         </div>
                     </div>
+                
                 </div>
-            
             </div>
-        </div>
-        
-    )
+            
+        )
+    }
+    
 }
-export default Register
+const mapStateToProps = () => {
+    return {};
+  };
+  
+  const mapDispatchToProps = {
+    // registerUser,
+  };
+  export default connect(mapStateToProps, mapDispatchToProps)(Register);
+  
